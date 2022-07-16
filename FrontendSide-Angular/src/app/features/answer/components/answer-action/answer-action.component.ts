b138267d9b80628ea7166e4549521c24e0dba7e1
HP <mamooli_persianblog@yahoo.com>
2022-07-16 20:59:21 +0200

    Tex changed.

--- FrontendSide-Angular/src/app/features/answer/components/answer-action/answer-action.component.ts
+++ FrontendSide-Angular/src/app/features/answer/components/answer-action/answer-action.component.ts
@@ -86,7 +86,7 @@ export class AnswerActionComponent implements OnInit {
         }
       );
     } else {
-      this.notifier.notify('error', 'Max File Size Exceeds');
+      this.notifier.notify('error', 'Maximum file size exceeded.');
     }
   }
 
